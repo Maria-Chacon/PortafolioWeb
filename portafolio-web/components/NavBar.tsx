e8ab1836e676bbe898a8f4662abd5b21cfa7b1cf
Maria-Chacon <123408968+Maria-Chacon@users.noreply.github.com>
2023-05-06 17:53:01 -0600

    Modification of logo

--- portafolio-web/components/NavBar.tsx
+++ portafolio-web/components/NavBar.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-
+import Logo from "/public/logo.png";
+import Image from "next/image";
 
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
-      <Navbar.Brand  className="ml-5 p-4 fs-3" href="#home">MACHAM</Navbar.Brand>
+      <Navbar.Brand className="ml-5 p-4 fs-3" href="#home">
+        <Image
+          src={Logo}
+          alt="Header Image"
+          placeholder="blur"
+          className="img-fluid"
+          width={100}
+          height={100}
+        />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" >
-        <Nav className="ml-auto" >
-          <Nav.Link href="#about" className="fs-4 m-3">Sobre mi</Nav.Link>
-          <Nav.Link href="#projects" className="fs-4 m-3">Mis Proyectos</Nav.Link>
-          <Nav.Link href="#contact" className="fs-4 m-3">Contacto</Nav.Link>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#about" className="fs-4 m-3">
+            Sobre mi
+          </Nav.Link>
+          <Nav.Link href="#projects" className="fs-4 m-3">
+            Mis Proyectos
+          </Nav.Link>
+          <Nav.Link href="#contact" className="fs-4 m-3">
+            Contacto
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
