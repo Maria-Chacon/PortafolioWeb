9731ae9db39aec57f6f9fb32a013a7a9589a6a8f
Maria-Chacon <123408968+Maria-Chacon@users.noreply.github.com>
2023-05-06 14:27:51 -0600

    Font size change

--- portafolio-web/components/NavBar.tsx
+++ portafolio-web/components/NavBar.tsx
@@ -5,13 +5,13 @@ import { Navbar, Nav } from "react-bootstrap";
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
-      <Navbar.Brand  className="ml-5 p-2" href="#home">MACHAM</Navbar.Brand>
+      <Navbar.Brand  className="ml-5 p-4 fs-3" href="#home">MACHAM</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" >
         <Nav className="ml-auto" >
-          <Nav.Link href="#about">Sobre mi</Nav.Link>
-          <Nav.Link href="#projects">Mis Proyectos</Nav.Link>
-          <Nav.Link href="#contact">Contacto</Nav.Link>
+          <Nav.Link href="#about" className="fs-4">Sobre mi</Nav.Link>
+          <Nav.Link href="#projects" className="fs-4">Mis Proyectos</Nav.Link>
+          <Nav.Link href="#contact" className="fs-4">Contacto</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
